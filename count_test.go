b4e40322bd01ad30024b0a9a0505c35b48ce03ed
ender <xuender@gmail.com>
2018-10-22 14:57:31 +0800

    test: change parallel

--- count_test.go
+++ count_test.go
@@ -6,7 +6,6 @@ func ExampleStream_Count() {
 	arr := []int{1, 2, 3, 4, 5}
 
 	i, err := New(arr).
-		Parallel().
 		Filter(func(i int) bool { return i > 1 }).
 		Count()
 
--- parallel_test.go
+++ parallel_test.go
@@ -1,11 +1,17 @@
 package stream
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func ExampleStream_Parallel() {
 	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	New(arr).
+	c, err := New(arr).
 		Parallel().
 		Filter(func(i int) bool { return i > 1 }).
-		ForEach(func(i int) { fmt.Println(i) })
+		Count()
+	fmt.Println(c, err)
+
+	// Output:
+	// 8 <nil>
 }
