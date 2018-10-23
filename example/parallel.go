451b9c7768ef1a2593010f9f49bb2818e8428d61
ender <xuender@gmail.com>
2018-10-23 10:58:29 +0800

    test: add examples

---
+++ example/parallel.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/xuender/go-stream"
+)
+
+func main() {
+	arr := []int{1, 2, 3, 4, 5, 6}
+	i, err := stream.New(arr).
+		Parallel().
+		Peek(func(i int) {
+			fmt.Println("peek1:", i)
+			time.Sleep(time.Second * time.Duration(i))
+		}).
+		Filter(func(i int) bool { return i > 1 }).
+		Peek(func(i int) {
+			fmt.Println("peek2:", i)
+			time.Sleep(time.Second * time.Duration(i))
+		}).
+		FindFirst()
+	fmt.Println(i, err)
+}
