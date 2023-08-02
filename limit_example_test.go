24bf813683a88fe3b58c4b44c4b2a5454cd08650
miaoyin <miaoyin@woda.com>
2023-08-02 13:58:43 +0800

    test: add limit test

--- limit_example_test.go
+++ limit_example_test.go
@@ -18,21 +18,3 @@ func ExampleBaseStream_Limit() {
 	// 1
 	// 2
 }
-
-func ExampleLimit() {
-	for num := range stream.Limit(stream.Range2Channel(1, 10), 0) {
-		fmt.Println(num)
-	}
-
-	// Output:
-}
-
-func ExampleLimit_out() {
-	for num := range stream.Limit(stream.Range2Channel(1, 2), 30) {
-		fmt.Println(num)
-	}
-
-	// Output:
-	// 0
-	// 1
-}
---
+++ limit_test.go
@@ -0,0 +1,29 @@
+package stream_test
+
+import (
+	"testing"
+
+	"github.com/xuender/go-stream"
+)
+
+func TestLimit(t *testing.T) {
+	t.Parallel()
+
+	sum := 0
+	for num := range stream.Limit(stream.Range2Channel(1, 10), 30) {
+		sum += num
+	}
+
+	if sum != 45 {
+		t.Errorf("maxSize=30 sum: %d", sum)
+	}
+
+	sum = 0
+	for num := range stream.Limit(stream.Range2Channel(1, 10), 0) {
+		sum += num
+	}
+
+	if sum != 0 {
+		t.Errorf("maxSize=0 sum: %d", sum)
+	}
+}
